99a0281b70c174533956fe804e0882790fc03e6b
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-01 12:55:15 +0400

    C# Fundamentals-Part1-Task10

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -179,7 +179,54 @@
                     Console.WriteLine("Invalid day number");
                     break;
             }
+            //Medium-task10 
 
+            Console.Write("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter operator +, -, *, /,%: ");
+            char op = char.Parse(Console.ReadLine());
+            switch (op) {
+                case '+':
+                    Console.WriteLine("Result: " + (num1 + num2));
+                    break;
+                case '-':
+                    Console.WriteLine("Result: " + (num1 - num2));
+                    break;
+                case '*':
+                    Console.WriteLine("Result: " + (num1 * num2));
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine("Result: " + (num1 / num2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+                    break;
+                case '%':
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine("Result: " + (num1 % num2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+             
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+
+            
+            
+            }
 
         }
     }
