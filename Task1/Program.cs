9df239d284b0bd6f32afe987a9abdb60581f59da
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-01 11:20:11 +0400

    C# Fundamentals-Part1-Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Easy-task 1
+            string name = "Bayan";
+            int age = 23;
+            double height = 154;
+            bool student = true;
+
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Age : " + age);
+            Console.WriteLine("Height : " + height);
+            Console.WriteLine("Student : " + student);
+
+
+
+
+
+        }
+    }
+}
