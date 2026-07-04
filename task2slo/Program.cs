091333538ce04652caa839dfecc26b738ffdb8cc
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-04 23:24:48 +0400

    C# Fundamentals - Part 2-task1

---
+++ task2slo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace task2slo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //easy-task1 
+            Console.Write("Enter a number: ");
+            int start = int.Parse(Console.ReadLine());
+
+            for (int i = start; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+        }
+    
+    }
+}
