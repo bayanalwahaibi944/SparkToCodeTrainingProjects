98b5ec5c61bf5cea93d16524bc5102f53a53bbe3
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-05 23:31:37 +0400

    C# Fundamentals - Part 3-task2

--- Task3Solu/Program.cs
+++ Task3Solu/Program.cs
@@ -14,6 +14,17 @@
             double difference = Math.Abs(num1 - num2);
 
             Console.WriteLine("Positive difference: " + difference);
+            //Easy-task2 
+            Console.Write("Enter a number: ");
+            double number = double.Parse(Console.ReadLine());
+
+            double s = Math.Pow(number, 2);
+            double sR = Math.Sqrt(number);
+
+            Console.WriteLine("Square: " + s);
+            Console.WriteLine("Square root: " + sR);
+
+
         }
     }
 }
