c6bbb8e68455476e00ed5aaefd31474515b0ad2c
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-09 11:52:15 +0400

    Fundamentals in C# - Part 5-task1

--- task5Solu/Program.cs
+++ task5Solu/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Easy-task1 
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write("Enter grade: ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
         }
     }
 }
