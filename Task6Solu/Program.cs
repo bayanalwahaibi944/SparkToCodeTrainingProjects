7b0d550682b107f91524a8cbc73a7a6d6f2d2983
bayanalwahaibi944 <bayanalwahaibi944@gmail.com>
2026-07-14 00:22:52 +0400

    create Bank Account Class

--- Task6Solu/Program.cs
+++ Task6Solu/Program.cs
@@ -1,10 +1,59 @@
 ﻿namespace Task6Solu
 {
-    internal class Program
+    internal class BankAccount
     {
-        static void Main(string[] args)
+        public int AccountNum { get; set; }
+
+        public string HolderName { get; set; }
+
+        public double Balance { get; set; }
+
+
+        public void Deposit(double amount)
         {
-            Console.WriteLine("Hello, World!");
+            Balance = Balance + amount;
+
+            SendEmail();
+        }
+
+
+        public void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance = Balance - amount;
+
+                SendEmail();
+            }
+        }
+
+
+        public double CheckBalance()
+        {
+            PrintInformation();
+
+            return Balance;
+        }
+
+
+        private void PrintInformation()
+        {
+            Console.WriteLine($"Holder Name: {HolderName}");
+
+            Console.WriteLine($"Balance: {Balance}");
+        }
+
+
+        private void SendEmail()
+        {
+            Console.WriteLine("Email notification sent.");
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+            }
         }
     }
 }
